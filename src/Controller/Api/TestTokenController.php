cccfb96077d560d8bd824879caae488a151de5d7
Benjamin Porta <pb402385@gmail.com>
2026-05-12 14:33:22 +0200

    Clean de ma page de test du token, ça fonctionne très bien avec Postman mais pas depuis le localhost donc je clean ce fichier car le souci ne provient pas de cette classe mais plutôt d'un souci en amont (security.yaml ou JwtAuthenticationEntryPoint)

--- src/Controller/Api/TestTokenController.php
+++ src/Controller/Api/TestTokenController.php
@@ -12,38 +12,9 @@ class TestTokenController extends AbstractController
     #[Route('/api/me', name: 'api_me', methods: ['GET'])]
     public function me(Request $request): JsonResponse
     {
-        dump('=== TestTokenController DÉBUT ===');
-
-        $token = ''.$request->headers->get('Authorization');
-        dump([
-            'Authorization Header' => $request->headers->get('Authorization'),
-            'getUser()' => $this->getUser(),
-            'token' => $token,
-            //$tokenHandler->getToken()' => $this->tokenHandler->getUserBadgeFrom($token),
-        ]);
-
 
         $user = $this->getUser();
-
-        if (!$user) {
-            dump('❌ $user non trouvé : ' . $user);
-        } else {
-            dump('✅ $user chargé (' . $user->getEmail());
-        }
-
-        //dd('TestTokenController');
-        //dd($this->getUser(), $user);
-
-/*
-        dump([
-            'UserIdentifier' => $user ? $user->getUserIdentifier() : null,
-            'User' => $user ? $user->getEmail() : null,
-            'Authorization Header' => $request->headers->get('Authorization'),
-            'All Headers' => $request->headers->all(),
-        ]);
-*/
-
-
+        
         if (!$user) {
             return $this->json([
                 'error' => 'Utilisateur non authentifié'
