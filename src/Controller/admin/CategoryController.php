086d9b17ab26b6f476d305be5a283f838a05e2aa
Benjamin Porta <pb402385@gmail.com>
2026-05-18 22:45:11 +0200

    On met quelques cotrôles sur l'envoit du formulaire (seul l'admin doit pouvoir lancer cette méthode

--- src/Controller/admin/CategoryController.php
+++ src/Controller/admin/CategoryController.php
@@ -36,10 +36,26 @@ final class CategoryController extends AbstractController
     #[Route('/add', name: 'add')]
     public function add(Request $request, EntityManagerInterface $em): Response
     {
+        $user = $this->getUser();
+        if ($user) {
+            // On est logué, on ne peut modifier l'utilisateur que si l'on est ADMIN ou si il s'agit de notre compte
+            if( !$this->isGranted('ROLE_ADMIN') ) {
+                $this->addFlash('danger',"Vous n'avez pas le droit d'ajouter une categorie'!");
+                return $this->redirectToRoute('admin.category.index');
+            }
+        }
 
         $category = new Category();
         $form = $this->createForm(CategoryType::class, $category);
         $form->handleRequest($request);
+        if($form->isSubmitted()){
+            // Utile pour débug quand le form ne passe pas les validateurs
+            dump([
+                'Submitted' => $form->isSubmitted(),
+                'Valid' => $form->isValid(),
+                'Errors' => $form->getErrors(true, true), // Voir toutes les erreurs
+            ]);
+        }
         if($form->isSubmitted() && $form->isValid()){
             $category->setCreatedAt(new \DateTimeImmutable());
             $category->setUpdatedAt(new \DateTime());
@@ -59,6 +75,15 @@ final class CategoryController extends AbstractController
     #[Route('/{id}/delete', name: 'delete', requirements: ['id' => '\d+'])]
     public function delete(Category $category, Request $request, EntityManagerInterface $em): Response
     {
+        $user = $this->getUser();
+        if ($user) {
+            // On est logué, on ne peut modifier l'utilisateur que si l'on est ADMIN ou si il s'agit de notre compte
+            if( !$this->isGranted('ROLE_ADMIN') ) {
+                $this->addFlash('danger',"Vous n'avez pas le droit de supprimer une categorie'!");
+                return $this->redirectToRoute('admin.category.index');
+            }
+        }
+
         $em->remove($category);
         $em->flush();
         $this->addFlash('success',"La catégorie a bien été supprimée");
